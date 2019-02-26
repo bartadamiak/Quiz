ec83b2ed8deec30d3339a9236d2b68d4dbb97b0e
bartadam <bart.adamiak@gmail.com>
2019-02-26 15:27:22 +0100

    First commit

---
+++ js/app.js
@@ -0,0 +1,64 @@
+const question = document.querySelector('.question p');
+const answer = document.querySelectorAll('.answer');
+const fakeAnswers = ["San Antonio", 'Kapsztad', 'Warszawa', 'Tokjo', 'Pekin', 'Sydney', 'Nashville', 'Wagadugu', 'Hanoi', 'Port Moresby'];
+
+
+const startBtn = document.querySelector('.start-btn');
+
+const countries = [
+    Nigeria = {
+    country: "Nigeria",
+    capital: "Abudża"
+    }, 
+    Meksyk = {
+    country: "Meksyk",
+    capital: "Mexico City"
+    },
+    Maroko = {
+        country: "Maroko",
+        capital: "Rabat"
+    },
+    Egipt = {
+        country: "Egipt",
+        capital: "Kair",
+    },
+    Rwanda = {
+        country: "Rwanda",
+        capital: "Kigali"
+    },
+    Zimbabwe = {
+        country: "Zimbabwe",
+        capital: "Harare"
+    },
+    Benin = {
+        country: "Benin",
+        capital: "Porto-Novo"
+    }
+];
+console.log(countries);
+
+let currentQuestionObject = "";
+let currentQuestionAnswer = "";
+let currentQuestion = "";
+
+
+
+
+startBtn.addEventListener('click', function(e) {
+    e.preventDefault;
+    currentQuestionObject = countries[Math.floor(Math.random()*countries.length)];
+    currentQuestionAnswer = currentQuestionObject.capital;
+    currentQuestion = currentQuestionObject.country;
+    question.innerText = currentQuestion;
+    answer[Math.floor(Math.random()*answer.length)].classList.add('correct');
+    document.querySelector('.correct').innerText = currentQuestionAnswer;
+    document.querySelector('.correct').classList.remove('fake');
+    let fake = document.querySelectorAll('.fake');
+    for (let i = 0; i < fake.length; i++) {
+        fake[i].innerText = fakeAnswers[Math.floor(Math.random()*fakeAnswers.length)];
+    }  
+    
+    
+    startBtn.classList.add('hidden');
+
+})
